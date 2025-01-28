2bbd60c6541972e1500726daa15db23e0069b02e
Moises Soto <35436843+moisoto@users.noreply.github.com>
2025-01-27 22:27:14 -0400

    Fix error on server.zsh script.

--- server.zsh
+++ server.zsh
@@ -2,7 +2,7 @@
 # You can access the Web UI at http://localhost:8080
 # Or use script call_endpoint.zsh to test chat completion functionality 
 if [ -z $1 ] ; then
-   model "unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF"
+   model="unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF"
 else
    model="$1"
 fi
